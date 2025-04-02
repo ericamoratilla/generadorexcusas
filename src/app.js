be2ba87a6069286d6c2f9fb94aadf154d099cb0f
ericamoratilla <erica.moratilla@gmail.com>
2025-04-02 10:31:14 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+
+  document.getElementById("excuse").innerHTML =
+    who[Math.floor(Math.random() * who.length)] + " " +
+    action[Math.floor(Math.random() * action.length)] + " " +
+    what[Math.floor(Math.random() * what.length)] + " " +
+    when[Math.floor(Math.random() * when.length)] + ".";
+
 };
